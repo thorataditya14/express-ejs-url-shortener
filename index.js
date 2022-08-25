98369a8f3bf89249523303dc532183cf9ebbf5d2
Aditya Thorat <thorat.aditya@outlook.com>
2022-08-25 21:38:29 +0530

    complete express-ejs-url-shortener

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const bodyParser = require("body-parser");
 const ShortUrl = require('./models/shortUrl')
 
+
 mongoose.connect(
     process.env.MONGO_URI, {
         useUnifiedTopology: true,
@@ -18,19 +19,24 @@ mongoose.connect(
 
 
 app.set('view engine', 'ejs')
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
+
 
 app.get('/', async (req, res) => {
     const shortUrls = await ShortUrl.find()
     res.render('index', { shortUrls: shortUrls })
 })
 
+
 app.post('/shortUrls', async (req, res) => {
     await ShortUrl.create({ full: req.body.fullUrl })
 
     res.redirect('/')
 })
+
 
 app.get('/:shortUrl', async (req, res) => {
     const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
@@ -42,6 +48,9 @@ app.get('/:shortUrl', async (req, res) => {
     res.redirect(shortUrl.full)
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+
+port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
